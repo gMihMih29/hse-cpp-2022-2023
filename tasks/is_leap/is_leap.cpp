17063e0e7217f113b8a700002d7e8e7d603a1194
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-04-13 16:24:31 +0300

    Solved task is_leap with two ifs

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,5 +1,11 @@
 #include "is_leap.h"
 
 bool IsLeap(int year) {
-    return true;
+    if (year % 400 == 0) {
+        return true;
+    }
+    if (year % 4 == 0 && year % 100 != 0) {
+        return true;
+    }
+    return false;
 }
