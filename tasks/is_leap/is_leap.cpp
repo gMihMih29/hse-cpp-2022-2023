21d39c12bd27296887300ea6e2f4ad767fddc1a1
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-04-13 16:36:50 +0300

    Made named constants globally visible

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,9 +1,10 @@
 #include "is_leap.h"
 
+const int FOUR = 4;
+const int HUNDRED = 100;
+const int FOUR_HUNDRED = 400;
+
 bool IsLeap(int year) {
-    const int FOUR = 4;
-    const int HUNDRED = 100;
-    const int FOUR_HUNDRED = 400;
     if (year % FOUR_HUNDRED == 0) {
         return true;
     }
