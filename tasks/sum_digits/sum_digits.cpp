026c0c484cc3a6e80eafea1f51d7587977eb4f11
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-04-13 17:11:44 +0300

    Created constant TEN

--- tasks/sum_digits/sum_digits.cpp
+++ tasks/sum_digits/sum_digits.cpp
@@ -1,10 +1,12 @@
 #include "sum_digits.h"
 
+const int64_t TEN = 10;
+
 int64_t SumOfDigits(int64_t number) {
     int64_t sum = 0;
     while (number) {
-        sum += number % 10;
-        number /= 10;
+        sum += number % TEN;
+        number /= TEN;
     }
     return sum;
 }
