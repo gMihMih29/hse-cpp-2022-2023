224f6099ec2c20f8367e7af2c41a4097e4a1cd89
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-04-20 17:19:59 +0300

    Second attempt

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -4,7 +4,7 @@ bool IsPalindrome(const std::string& str) {
     size_t l = 0;
     size_t r = str.length() - 1;
     while (l < r) {
-        while (str[l] == ' ' && l != str.length()) {
+        while (str[l] == ' ' && l != str.length() - 1) {
             ++l;
         }
         while (str[r] == ' ' && r != 0) {
