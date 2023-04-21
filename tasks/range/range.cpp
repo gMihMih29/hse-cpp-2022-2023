d053a0748b9606c32b4a2270494a0f7ddfba5142
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-04-21 09:24:53 +0300

    Solved task

--- tasks/range/range.cpp
+++ tasks/range/range.cpp
@@ -1,5 +1,19 @@
 #include "range.h"
 
 std::vector<int> Range(int from, int to, int step) {
-    return {};
+    if ((to - from) * static_cast<int64_t>(step) <= 0) {
+        return std::vector<int>(0);
+    }
+    std::vector<int> result;
+    result.reserve(std::abs(to - from) / std::abs(step));
+    if (step > 0) {
+        for (;from < to; from += std::abs(step)) {
+            result.push_back(from);
+        }
+    } else {
+        for (;from > to; from += step) {
+            result.push_back(from);
+        }
+    }
+    return result;
 }
