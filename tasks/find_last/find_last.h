c8ff557eaac4647da8f4199df08cbbc722452c9c
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-04-25 19:17:56 +0300

    Solved task find_last

--- tasks/find_last/find_last.h
+++ tasks/find_last/find_last.h
@@ -2,5 +2,10 @@
 
 template <class Iterator, class T>
 Iterator FindLast(Iterator first, Iterator last, const T& val) {
-    return first;
+    for (Iterator i = last - 1; i >= first; --i) {
+        if (*i == val) {
+            return i;
+        }
+    }
+    return last;
 }
