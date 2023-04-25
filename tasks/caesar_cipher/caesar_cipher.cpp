6c6f8b0f430f35d24feb21dee84883fa862d88e9
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-04-25 19:11:33 +0300

    Fixed type problems 2

--- tasks/caesar_cipher/caesar_cipher.cpp
+++ tasks/caesar_cipher/caesar_cipher.cpp
@@ -1,16 +1,16 @@
 #include "caesar_cipher.h"
 
-const char ALPHABET_LENGTH = 'Z' - 'A' + 1;
+const int ALPHABET_LENGTH = 'Z' - 'A' + 1;
 
 std::string Decipher(const std::string& cipher, size_t shift) {
     std::string result = cipher;
-    char symbol_number;
+    int symbol_number = 0;
     shift %= ALPHABET_LENGTH;
     for (size_t i = 0; i < cipher.length(); ++i) {
         symbol_number = cipher[i] - 'A';
-        symbol_number += ALPHABET_LENGTH - shift;
+        symbol_number += ALPHABET_LENGTH - static_cast<int>(shift);
         symbol_number %= ALPHABET_LENGTH;
-        result[i] = symbol_number + 'A';
+        result[i] = static_cast<char>(symbol_number + 'A');
     }
     return result;
 }
