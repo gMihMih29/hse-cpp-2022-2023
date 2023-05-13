4bb7aeee1b3b65d2c1cd2fe8af676b82034928d4
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-05-13 12:02:41 +0300

    Solved task

--- tasks/remove/remove.h
+++ tasks/remove/remove.h
@@ -1,4 +1,13 @@
 #pragma once
 
 template <typename T, typename U>
-T Remove(T begin, T end, U value);
+T Remove(T begin, T end, U value) {
+    T values_to_remove_iter = begin;
+    for (; begin < end; ++begin) {
+        if (*begin != value) {
+            std::swap(*values_to_remove_iter, *begin);
+            ++values_to_remove_iter;
+        }
+    }
+    return values_to_remove_iter;
+}
