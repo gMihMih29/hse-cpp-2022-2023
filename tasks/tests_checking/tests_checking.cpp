3ac7049ec26d592d5aec898ec7b314815ed00c22
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-05-19 17:48:23 +0300

    Solved task

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -2,5 +2,18 @@
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    return {};
+    std::deque<std::string> papers;
+    for (const auto& i : student_actions) {
+        if (i.side == Side::Top) {
+            papers.push_front(i.name);
+        } else {
+            papers.push_back(i.name);
+        }
+    } 
+    std::vector<std::string> answer;
+    answer.reserve(queries.size());
+    for (const auto& i : queries) {
+        answer.push_back(papers.at(i - 1));
+    }
+    return answer;
 }
