bfdb3fa1193a695b12c55e4f0d8f7667d1516236
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-05-19 18:19:31 +0300

    Added normalization

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -1,35 +1,50 @@
 #include "word2vec.h"
 
 #include <vector>
+#include <cmath>
+
+const double EPS = 1e-9;
 
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
     size_t dimensions = vectors[0].size();
-    int max = 0;
-    int count_max = 0;
-    int current_result = 0;
-    bool is_first = true;
+    double current_result = 0;
+    std::vector<double> self_scalar_prods;
+    std::vector<double> first_scalar_prods;
+    self_scalar_prods.reserve(vectors.size());
+    first_scalar_prods.reserve(vectors.size());
     for (const auto& v : vectors) {
         current_result = 0;
-        for (size_t i = 0; i < dimensions; ++i) {
-            current_result += v[i] * vectors[0][i];
+        for (size_t j = 0; j < dimensions; ++j) {
+            current_result += v[j] * v[j];
         }
-        if (max < current_result || is_first) {
+        self_scalar_prods.push_back(current_result);
+    }
+    for (const auto& v : vectors) {
+        current_result = 0;
+        for (size_t j = 0; j < dimensions; ++j) {
+            current_result += v[j] * vectors[0][j];
+        }
+        first_scalar_prods.push_back(current_result);
+    }
+    double max = -2;
+    int count_max = 0;
+    bool is_first = true;
+    for (size_t i = 1; i < vectors.size(); ++i) {
+        current_result = first_scalar_prods[i] / (std::sqrt(self_scalar_prods[i]) * std::sqrt(self_scalar_prods[0]));
+        if (std::abs(max - current_result) < EPS) {
+            ++count_max;
+        } else if (max < current_result || is_first) {
             max = current_result;
             count_max = 1;
             is_first = false;
-        } else if (max == current_result) {
-            ++count_max;
         }
     }
     std::vector<std::string> answer;
     answer.reserve(count_max);
     for (size_t i = 0; i < vectors.size(); ++i) {
-        current_result = 0;
-        for (size_t j = 0; j < dimensions; ++j) {
-            current_result += vectors[i][j] * vectors[0][j];
-        }
-        if (max == current_result) {
+        current_result = first_scalar_prods[i] / (std::sqrt(self_scalar_prods[i]) * std::sqrt(self_scalar_prods[0]));
+        if (std::abs(max - current_result) < EPS) {
             answer.push_back(words[i]);
         }
     }
