15f984b4bed9ae2143524d8e2692e610f57d7bdf
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-05-20 09:03:57 +0300

    Fixed task

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -3,42 +3,30 @@
 #include <cmath>
 #include <vector>
 
-const double EPS = 1e-9;
-
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
     if (vectors.empty()) {
         return std::vector<std::string>();
     }
     size_t dimensions = vectors[0].size();
-    double current_result = 0;
-    std::vector<double> self_scalar_prods;
-    std::vector<double> first_scalar_prods;
-    self_scalar_prods.reserve(vectors.size());
-    first_scalar_prods.reserve(vectors.size());
-    for (size_t i = 0; i < vectors.size(); ++i) {
-        current_result = 0;
-        for (size_t j = 0; j < dimensions; ++j) {
-            current_result += vectors[i][j] * vectors[i][j];
-        }
-        self_scalar_prods.push_back(current_result);
-    }
+    int64_t current_result = 0;
+    std::vector<int64_t> scalar_prods;
+    scalar_prods.reserve(vectors.size());
     for (size_t i = 0; i < vectors.size(); ++i) {
         current_result = 0;
         for (size_t j = 0; j < dimensions; ++j) {
             current_result += vectors[i][j] * vectors[0][j];
         }
-        first_scalar_prods.push_back(current_result);
+        scalar_prods.push_back(current_result);
     }
-    double max = -2;
+    int64_t max = scalar_prods[1];
     int count_max = 0;
     bool is_first = true;
-    for (size_t i = 1; i < vectors.size(); ++i) {
-        current_result = first_scalar_prods[i] / (std::sqrt(self_scalar_prods[i]) * std::sqrt(self_scalar_prods[0]));
-        if (std::abs(max - current_result) < EPS) {
+    for (size_t i = 2; i < vectors.size(); ++i) {
+        if (max == scalar_prods[i]) {
             ++count_max;
-        } else if (max < current_result || is_first) {
-            max = current_result;
+        } else if (max < scalar_prods[i] || is_first) {
+            max = scalar_prods[i];
             count_max = 1;
             is_first = false;
         }
@@ -46,8 +34,7 @@ std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
     std::vector<std::string> answer;
     answer.reserve(count_max);
     for (size_t i = 1; i < vectors.size(); ++i) {
-        current_result = first_scalar_prods[i] / (std::sqrt(self_scalar_prods[i]) * std::sqrt(self_scalar_prods[0]));
-        if (std::abs(max - current_result) < EPS) {
+        if (max == scalar_prods[i]) {
             answer.push_back(words[i]);
         }
     }
