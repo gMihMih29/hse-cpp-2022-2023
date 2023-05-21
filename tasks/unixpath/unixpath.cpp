eed541c88992177f9f778b13644c688c1584edda
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-05-21 11:03:34 +0300

    Solved task

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -1,5 +1,26 @@
 #include "unixpath.h"
 
 std::string NormalizePath(std::string_view current_working_dir, std::string_view path) {
-    return "";
+    size_t left_path_index = 0;
+    size_t right_path_index = 0;
+    std::string answer = static_cast<std::string>(current_working_dir);
+    while (right_path_index < path.length()) {
+        while (right_path_index < path.length() && path[right_path_index] != '/') {
+            ++right_path_index;
+        }
+        if (path.substr(left_path_index, right_path_index - left_path_index) == "..") {
+            answer = answer.substr(0, answer.find_last_of('/'));
+        } else if (path.substr(left_path_index, right_path_index - left_path_index) != ".") {
+            answer += "/";
+            answer += path.substr(left_path_index, right_path_index - left_path_index);
+        }
+        while (right_path_index < path.length() && path[right_path_index] == '/') {
+            ++right_path_index;
+        }
+        left_path_index = right_path_index;
+    }
+    if (answer.empty()) {
+        answer = "/";
+    }
+    return answer;
 }
