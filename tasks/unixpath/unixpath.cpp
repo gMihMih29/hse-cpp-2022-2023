1b07ba6477e1522819869182b53a5033fe9b6854
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-05-21 11:26:53 +0300

    Fixed some problems

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -7,20 +7,20 @@ std::string NormalizePath(std::string_view current_working_dir, std::string_view
     size_t left_path_index = 0;
     size_t right_path_index = 0;
     std::string answer;
-    if (path[0] == '/') {
-        answer = "/";
-    } else {
+    if (path[0] != '/') {
         answer = static_cast<std::string>(current_working_dir);
     }
     while (right_path_index < path.length()) {
         while (right_path_index < path.length() && path[right_path_index] != '/') {
             ++right_path_index;
         }
-        if (path.substr(left_path_index, right_path_index - left_path_index) == "..") {
-            answer = answer.substr(0, answer.find_last_of('/'));
-        } else if (path.substr(left_path_index, right_path_index - left_path_index) != ".") {
-            answer += "/";
-            answer += path.substr(left_path_index, right_path_index - left_path_index);
+        if (left_path_index != right_path_index) {
+            if (path.substr(left_path_index, right_path_index - left_path_index) == "..") {
+                answer = answer.substr(0, answer.find_last_of('/'));
+            } else if (path.substr(left_path_index, right_path_index - left_path_index) != ".") {
+                answer += "/";
+                answer += path.substr(left_path_index, right_path_index - left_path_index);
+            }
         }
         while (right_path_index < path.length() && path[right_path_index] == '/') {
             ++right_path_index;
