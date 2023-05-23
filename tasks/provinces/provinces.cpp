795dd77e175cc355e4a1258bd1b7b1a4e4b8139a
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-05-23 11:10:59 +0300

    Solved task

--- tasks/provinces/provinces.cpp
+++ tasks/provinces/provinces.cpp
@@ -2,17 +2,17 @@
 
 int64_t CountPassports(const std::vector<int>& provinces) {
     int64_t answer = 0;
-    std::multiset<int> s;
+    std::multiset<int64_t> s;
     for (const int& p : provinces) {
         s.insert(p);
     }
-    int val1 = 0;
-    int val2 = 0;
+    int64_t val1 = 0;
+    int64_t val2 = 0;
     while (s.size() != 1) {
         val1 = *s.begin();
-        s.erase(val1);
+        s.erase(s.begin());
         val2 = *s.begin();
-        s.erase(val2);
+        s.erase(s.begin()); 
         answer += val1 + val2;
         s.insert(val1 + val2);
     }
