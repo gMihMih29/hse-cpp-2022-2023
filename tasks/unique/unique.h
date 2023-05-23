5cc6da33205775b442508689e3483946e56c4c1e
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-05-23 14:11:41 +0300

    Solved task

--- tasks/unique/unique.h
+++ tasks/unique/unique.h
@@ -1,4 +1,15 @@
 #pragma once
 
 template <typename T>
-T Unique(T begin, T end);
+T Unique(T begin, T end) {
+    T unique_end = begin;
+    bool flag = begin != end;
+    ++begin;
+    for (; begin < end; ++begin) {
+        if (*unique_end != *begin) {
+            ++unique_end;
+            std::swap(*unique_end, *begin);
+        }
+    }
+    return unique_end + (flag ? 1 : 0);
+}
