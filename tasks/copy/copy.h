9d7b258ec2c8e2ef314f87d66b168250716e531d
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-05-23 11:37:21 +0300

    Trying to solve problem

--- tasks/copy/copy.h
+++ tasks/copy/copy.h
@@ -12,9 +12,8 @@ template <typename T, typename U>
 U CopyBackward(T src_begin, T src_end, U dest) {
     --src_end;
     --dest;
-    for (; src_begin <= src_end; --src_end) {
+    for (; src_begin <= src_end; --src_end, --dest;) {
         *dest = *src_end;
-        --dest;
     }
-    return dest;
+    return ++dest;
 }
