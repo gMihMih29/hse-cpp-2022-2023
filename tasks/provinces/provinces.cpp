4fdb63026b7d3d93f3c3969128d954bb462c31d6
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-05-25 13:32:18 +0300

    Fixed problems

--- tasks/provinces/provinces.cpp
+++ tasks/provinces/provinces.cpp
@@ -2,19 +2,19 @@
 
 int64_t CountPassports(const std::vector<int>& provinces) {
     int64_t answer = 0;
-    std::multiset<int64_t> s;
-    for (const int& p : provinces) {
-        s.insert(p);
+    std::multiset<int64_t> mergable_provinces;
+    for (const int& population : provinces) {
+        mergable_provinces.insert(population);
     }
     int64_t val1 = 0;
     int64_t val2 = 0;
-    while (s.size() != 1) {
-        val1 = *s.begin();
-        s.erase(s.begin());
-        val2 = *s.begin();
-        s.erase(s.begin());
+    while (mergable_provinces.size() >= 1) {
+        val1 = *mergable_provinces.begin();
+        mergable_provinces.erase(mergable_provinces.begin());
+        val2 = *mergable_provinces.begin();
+        mergable_provinces.erase(mergable_provinces.begin());
         answer += val1 + val2;
-        s.insert(val1 + val2);
+        mergable_provinces.insert(val1 + val2);
     }
     return answer;
 }
