c11936818f9a4a866e204aeca13e47e3c909a6da
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-06-12 17:23:33 +0300

    In progress

--- tasks/smart-ptr/smart_ptr.h
+++ tasks/smart-ptr/smart_ptr.h
@@ -3,17 +3,46 @@
 #include <string>
 
 template <class T>
+class ControlBlock {
+private:
+    T* ptr_;
+    size_t cnt_shared_;
+    size_t cnt_weak_;
+};
+
+template <class T>
 class WeakPtr;
 
 template <class T>
 class SharedPtr {
 public:
+    SharedPtr();
+
     SharedPtr(const WeakPtr<T>& rhs);
+
+    SharedPtr(T*);
+
+    T* Get();
+
+    T operator*();
+
+    void Reset(T*);
+
+private:
+    T* ptr_;
+    ControlBlock* ctrl_block_;
 };
 
 template <class T>
 class WeakPtr {
 public:
+    WeakPtr();
+
+    WeakPtr(const SharedPtr<T>& rhs);
+
+    SharedPtr Lock();
+
+    bool IsExpired();
 };
 
 template <class T>
