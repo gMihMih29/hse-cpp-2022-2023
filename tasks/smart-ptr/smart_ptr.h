561b64183b4dbc31c798c407c67f3c7e44f67de3
Mikhail Getmanov <mmgetmanov@gmail.com>
2023-06-15 19:46:43 +0300

    Solved task

--- tasks/smart-ptr/smart_ptr.h
+++ tasks/smart-ptr/smart_ptr.h
@@ -3,12 +3,7 @@
 #include <string>
 
 template <class T>
-class ControlBlock {
-private:
-    T* ptr_;
-    size_t cnt_shared_;
-    size_t cnt_weak_;
-};
+class ControlBlock;
 
 template <class T>
 class WeakPtr;
@@ -16,35 +11,161 @@ class WeakPtr;
 template <class T>
 class SharedPtr {
 public:
-    SharedPtr();
+    friend ControlBlock<T>;
+    friend WeakPtr<T>;
 
-    SharedPtr(const WeakPtr<T>& rhs);
+public:
+    SharedPtr() : ptr_(nullptr), ctrl_block_(new ControlBlock<T>()) {
+    }
 
-    SharedPtr(T*);
+    SharedPtr(const SharedPtr<T>& rhs) : ptr_(rhs.ctrl_block_->Get()), ctrl_block_(rhs.ctrl_block_) {
+        ctrl_block_->AddSharedPtr();
+    }
 
-    T* Get();
+    SharedPtr(const WeakPtr<T>& rhs) : ptr_(rhs.ctrl_block_->Get()), ctrl_block_(rhs.ctrl_block_) {
+        ctrl_block_->AddSharedPtr();
+    }
 
-    T operator*();
+    SharedPtr(ControlBlock<T>* ctrl) : ptr_(ctrl->Get()), ctrl_block_(ctrl) {
+        ctrl_block_->AddSharedPtr();
+    }
 
-    void Reset(T*);
+    SharedPtr(T* ptr) : ptr_(ptr) {
+        ctrl_block_ = new ControlBlock<T>(*this);
+    }
+
+    ~SharedPtr() {
+        if (ctrl_block_) {
+            ctrl_block_->RemoveSharedPtr();
+            if (ctrl_block_->IsExpired()) {
+                delete ctrl_block_;
+            }
+        }
+    }
+
+    SharedPtr& operator=(const SharedPtr& rhv) {
+        ctrl_block_->RemoveSharedPtr();
+        if (ctrl_block_->IsExpired()) {
+            delete ctrl_block_;
+        }
+        ptr_ = rhv.ptr_;
+        ctrl_block_ = rhv.ctrl_block_;
+        ctrl_block_->AddSharedPtr();
+        return *this;
+    }
+
+    SharedPtr& operator=(SharedPtr&& rhv) {
+        ctrl_block_->RemoveSharedPtr();
+        if (ctrl_block_->IsExpired()) {
+            delete ctrl_block_;
+        }
+        ptr_ = rhv.ptr_;
+        ctrl_block_ = rhv.ctrl_block_;
+        rhv.ptr_ = nullptr;
+        rhv.ctrl_block_ = nullptr;
+        return *this;
+    }
+
+    T* Get() {
+        return ptr_;
+    }
+
+    T operator*() {
+        return *ptr_;
+    }
+
+    void Reset(T* new_ptr) {
+        ctrl_block_->RemoveSharedPtr();
+        if (ctrl_block_->IsExpired()) {
+            delete ctrl_block_;
+        }
+        ptr_ = new_ptr;
+        ctrl_block_ = new ControlBlock(*this);
+    }
 
 private:
     T* ptr_;
-    ControlBlock* ctrl_block_;
+    ControlBlock<T>* ctrl_block_;
 };
 
 template <class T>
 class WeakPtr {
 public:
-    WeakPtr();
+    friend ControlBlock<T>;
+    friend SharedPtr<T>;
 
-    WeakPtr(const SharedPtr<T>& rhs);
+public:
+    WeakPtr() : ctrl_block_(nullptr) {
+    }
 
-    SharedPtr Lock();
+    WeakPtr(const SharedPtr<T>& rhs) : ctrl_block_(rhs.ctrl_block_) {
+        ctrl_block_->AddWeakPtr();
+    }
 
-    bool IsExpired();
+    SharedPtr<T> Lock() {
+        if (!ctrl_block_ || IsExpired()) {
+            return SharedPtr<T>();
+        }
+        return SharedPtr<T>(ctrl_block_);
+    }
+
+    bool IsExpired() const {
+        return !ctrl_block_ || !ctrl_block_->Get();
+    }
+private:
+    ControlBlock<T>* ctrl_block_;
 };
 
 template <class T>
-SharedPtr<T>::SharedPtr(const WeakPtr<T>& rhs) {
-}
+class ControlBlock {
+public:
+    ControlBlock() : ptr_(nullptr), cnt_shared_(1), cnt_weak_(0) {
+    }
+
+    ControlBlock(const SharedPtr<T>& ptr) : ptr_(ptr.ptr_), cnt_shared_(1), cnt_weak_(0) {
+    }
+
+    ControlBlock(const WeakPtr<T>& ptr) : ptr_(ptr.ptr_), cnt_shared_(0), cnt_weak_(1) {
+    }
+
+    ~ControlBlock() {
+        delete ptr_;
+    }
+
+    void AddSharedPtr() {
+        ++cnt_shared_;
+    }
+
+    void AddWeakPtr() {
+        ++cnt_weak_;
+    }
+
+    void RemoveSharedPtr() {
+        if (cnt_shared_ != 0) {
+            --cnt_shared_;
+        }
+        if (cnt_shared_ == 0) {
+            delete ptr_;
+            ptr_ = nullptr;
+        }
+    }
+
+    void RemoveWeakPtr() {
+        if (cnt_weak_ != 0) {
+            --cnt_weak_;
+        }
+    }
+
+    T* Get() {
+        return ptr_;
+    }
+    
+    bool IsExpired() {
+        return cnt_shared_ == 0 && cnt_weak_ == 0;
+    }
+
+private:
+    T* ptr_;
+    size_t cnt_shared_;
+    size_t cnt_weak_;
+};
